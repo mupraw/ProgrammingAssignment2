8ab088108bd0ca9e9316f706db242417ffa4f6cf
mupraw <57252421+mupraw@users.noreply.github.com>
2020-11-04 10:03:01 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,25 @@
-## Put comments here that give an overall description of what your
+## Below is the function to produce an inverse of 2 x 2 matrix.  
 ## functions do
 
-## Write a short comment describing this function
+## The function calculate inversible 2x2 matrix that comes with additional feature to prevent any error such as dimension of matrix is not 2x2 or determinat of the matrix is zero 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+y <- dim(x)
+        
+if(y[1] != 2 & y[2] != 2){
+  "This is not 2x2 matrix"      
+}else{
+  det <- x[1,1]*x[2,2]-x[2,1]*x[1,2]
+        if(det == 0){
+           "Matrix is singular"     
+        }else {X <- matrix(c(x[2,2],-x[2,1],-x[1,2],x[1,1]), nrow = 2, ncol = 2)
+  invX <- 1/det*X
+                }
+  
+  
+  
+}
+        return(invX)
 }
 
 
